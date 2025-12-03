e962fd03924c2960c2fb627413221657c673f967
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-12-03 20:13:38 +0200

    Update apps

--- 4_1_dala.py
+++ 4_1_dala.py
@@ -1,23 +1,27 @@
 import requests
 import json
-import statistics
-import random
-from collections import defaultdict
+
 
 url1 = requests.get("https://pro2025.azurewebsites.net/journals")
 url2 = requests.get("https://pro2025.azurewebsites.net/journals?year_established=2000")
 url3 = requests.get("https://pro2025.azurewebsites.net/books?genre=Fantasy&year_published=2020")
-
-
-url41 = requests.get("https://pro2025.azurewebsites.net/books")
-def check_status(url41):
+url4_1 = requests.get("https://pro2025.azurewebsites.net/books")
+def check_status(url4_1):
     try:
-        resp = requests.get(url41, timeout=10)
+        resp = requests.get(url4_1, timeout=10)
         if resp.status_code == 200:
-            print(f"Savienojums ar {url41}: statusa kods 200 — veiksmīgi saņemti dati.")
+            print(f"Savienojums ar {url4_1}: statusa kods 200 — veiksmīgi saņemti dati.")
         else:
-            print(f"Savienojums ar {url41}: statusa kods {resp.status_code} — pārbaudiet URL vai serveri.")
+            print(f"Savienojums ar {url4_1}: statusa kods {resp.status_code} — pārbaudiet URL vai serveri.")
         return resp
     except Exception as e:
-        print(f"Kļūda, mēģinot piekļūt {url41}: {e}")
+        print(f"Kļūda, mēģinot piekļūt {url4_1}: {e}")
         raise
+    
+response = check_status("https://pro2025.azurewebsites.net/books")
+data = response.json()
+
+
+url4_1 = requests.get("https://pro2025.azurewebsites.net/books")
+for book in data:
+    print(book)
